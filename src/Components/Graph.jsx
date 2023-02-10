c7156314d2be2929a2217556e0d41fde1f1665fc
piqueblinder <preetkhatri07@gmail.com>
2023-02-11 01:14:52 +0530

    added graphs

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
+import ReactApexChart from "react-apexcharts";
+
 
 function Graph() {
+
+  const [example,setExample]=React.useState({
+    series: [{
+      data: [44, 55, 41, 64, 22, 43, 21]
+    }, {
+      data: [53, 32, 33, 52, 13, 44, 32]
+    }, {
+      data: [50, 3, 23, 25, 53, 94, 30]
+    }
+  ],
+    options: {
+      chart: {
+        type: 'bar',
+        height: 430
+      },
+      plotOptions: {
+        bar: {
+          vertical: true,
+          dataLabels: {
+            position: 'top',
+          },
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        offsetX: -6,
+        style: {
+          fontSize: '12px',
+          colors: ['#fff']
+        }
+      },
+      stroke: {
+        show: true,
+        width: 1,
+        colors: ['#fff']
+      },
+      tooltip: {
+        shared: true,
+        intersect: false
+      },
+      xaxis: {
+        categories: [2001, 2002, 2003, 2004, 2005, 2006, 2007],
+      },
+    },
+  });
+
+
   return (
     <>
-        <h1>graph</h1>
+      <h1>graph</h1>
+
+      <ReactApexChart 
+      options={example.options} 
+      series={example.series} 
+      type="bar" 
+      height={430} />
     </>
   )
 }
 
-export default Graph
+export default Graph;
