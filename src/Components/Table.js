fbdf0efce0c7825787095068a42c5d1f5627eed7
Rutwik Dhale <rutwik2514@gmail.com>
2023-02-10 23:38:06 +0530

    Display table filtered according to branches

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,12 +1,16 @@
 import React from "react";
 import data from "../Data";
 export default function Table() {
-  const [show, setShow] = React.useState(false);
+  const [showBranch, setShowBranch] = React.useState(false);
+  const [showData, setShowData] = React.useState(false);
   const [roll, setRoll] = React.useState([]);
   const [name, setName] = React.useState([]);
   const [year, setYear] = React.useState([]);
   const [branch, setBranch] = React.useState([]);
+  const [whichYear, setWhichYear] = React.useState();
+  const [branchElements, setBranchElements] = React.useState([]);
   const numberOfYears = [];
+  const numberOfBranches = [];
   const elements = data.map((myList) => numberOfYears.push(myList.year));
   let years = [...new Set(numberOfYears)];
   const yearElements = years.map((myList) => (
@@ -14,7 +18,27 @@ export default function Table() {
       {myList}
     </button>
   ));
+
   function handleClick(params) {
+    setWhichYear(params);
+  }
+  React.useEffect(() => {
+    let tempData = [];
+    const tempElements = data.map((myList) => {
+      if (whichYear == myList.year) {
+        numberOfBranches.push(myList.branch);
+        tempData.push(myList);
+      }
+    });
+    let branches = [...new Set(numberOfBranches)];
+    const tempBranchElements = branches.map((myList) => (
+      <button onClick={() => handleClick2(myList)}>{myList}</button>
+    ));
+    setBranchElements(tempBranchElements);
+    setShowBranch(true);
+  }, [whichYear]);
+
+  function handleClick2(props) {
     setRoll([]);
     setBranch([]);
     setYear([]);
@@ -24,42 +48,58 @@ export default function Table() {
     const tempBranch = [];
     const tempName = [];
     const tempElements = data.map((myList) => {
-      if (params == myList.year) {
-        tempRoll.push(<li key={myList.roll} style={{listStyle : 'none'}}>{myList.roll}</li>);
-        tempName.push(<li key={myList.roll} style={{listStyle : 'none'}}>{myList.name}</li>);
-        tempYear.push(<li key={myList.roll} style={{listStyle : 'none'}}>{myList.year}</li>);
-        tempBranch.push(<li key={myList.roll} style={{listStyle : 'none'}}>{myList.branch}</li>);
+      if (props == myList.branch && myList.year == whichYear) {
+        tempRoll.push(
+          <li key={myList.roll} style={{ listStyle: "none" }}>
+            {myList.roll}
+          </li>
+        );
+        tempName.push(
+          <li key={myList.roll} style={{ listStyle: "none" }}>
+            {myList.name}
+          </li>
+        );
+        tempYear.push(
+          <li key={myList.roll} style={{ listStyle: "none" }}>
+            {myList.year}
+          </li>
+        );
+        tempBranch.push(
+          <li key={myList.roll} style={{ listStyle: "none" }}>
+            {myList.branch}
+          </li>
+        );
       }
+      setBranch(tempBranch);
+      setRoll(tempRoll);
+      setName(tempName);
+      setYear(tempYear);
+      setShowData(true);
     });
-    setBranch(tempBranch);
-    setRoll(tempRoll);
-    setName(tempName);
-    setYear(tempYear);
-    setShow(true);
   }
 
   return (
     <>
       {yearElements}
-
-      {show && (
+      {showBranch && branchElements}
+      {showData && (
         <section className="d-flex justify-content-center align-items-center">
           <div
             className="col-1 d-flex justify-content-center align-items-center"
             style={{
               minHeight: "10vh",
               flexDirection: "column",
-              border : '1px solid black'
+              border: "1px solid black",
             }}
           >
-          {roll}
+            {roll}
           </div>
           <div
             className="col-5 d-flex justify-content-center align-items-center"
             style={{
               minHeight: "10vh",
               flexDirection: "column",
-              border : '1px solid black'
+              border: "1px solid black",
             }}
           >
             {name}
@@ -69,7 +109,7 @@ export default function Table() {
             style={{
               minHeight: "10vh",
               flexDirection: "column",
-              border : '1px solid black'
+              border: "1px solid black",
             }}
           >
             {year}
@@ -79,7 +119,7 @@ export default function Table() {
             style={{
               minHeight: "10vh",
               flexDirection: "column",
-              border : '1px solid black'
+              border: "1px solid black",
             }}
           >
             {branch}
