a75966d9d9f41be1dd28cfbe27840a0a16dc85e0
Rutwik Dhale <rutwik2514@gmail.com>
2023-02-11 00:05:04 +0530

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./App.css"
 import Table from './Components/Table';
+// import Form from './Components/Form';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Table />} />
-
-
         </Routes>
+        {/* <Form /> */}
       </BrowserRouter>
     </>
   )
