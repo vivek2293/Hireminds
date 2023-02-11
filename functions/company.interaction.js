1594b2f4be496c974cc02773619b3c9ddeebbbf5
vivek2293 <vivek22935@gmail.com>
2023-02-11 12:48:37 +0530

    Added backend for rendering shortlisted candidates

--- functions/company.interaction.js
+++ functions/company.interaction.js
@@ -1,8 +1,7 @@
 const shortlistedCandidates = require("../model/interaction");
 const studentData = require("../model/student");
-const mail = require("../middlewares/mailer");
+const { mail } = require("../middlewares/mailer");
 const jwt = require("jsonwebtoken");
-const server = "http://localhost:3000/shortlisted";
 
 const shortlistedCandidateslist = async(req, res) => {
     const { companyName } = req.body
@@ -40,16 +39,18 @@ const createListOfShortlistedCandidates = async(arr, companyName, email) => {
         companyName,
         email,
         _id: task._id
-    }, process.env.JWT_SECRET, { expiresIn: "30 day"});
+    }, process.env.JWT_SECRET, { expiresIn: "15 day"});
     // console.log(token)
+    const subject = "List of shortlisted candidates."
     const message = 
     `
     Hello Sir, 
     We are providing you the list of the shortlisted students on the basis of the shortlisting criteria provided by you.
-    Link: ${server + "?token" + token}.
+    This link will only be valid for 15 days.
+    Link: ${process.env.CLIENT + "?token=" + token}
     `
     console.log(message)
-    // mail( email, subject, )
+    // mail("preetkhatri07@gmail.com", subject, message);
 }
 
 const getEligibileCandidateList = async(req, res) => {
@@ -84,5 +85,27 @@ const getEligibileCandidateList = async(req, res) => {
     });
 }
 
+const renderShortlistedCandidate = async(req, res) => {
+    const { token } = req.body;
+    console.log(token);
+    const payload = jwt.decode( token , process.env.JWT_SECRET, (err, authData) => {
+        console.log(authData);
+    })
+    
+    //find
+    try{
+        const query = await shortlistedCandidates.find({ _id: payload._id }).limit(1);
+        const listOfAllCandidates = query[0].list;
+        studentData.find().where("_id").in(listOfAllCandidates).select("rollNo name branch gender degree CGPA email contactNo linkedIn github resumeLink").exec((err, records) => {
+            if(err) return res.status(400).json({ "msg": err });
+            return res.status(200).json(records);
+        });
+    }
+    catch (err){
+        return res.status(400).json({ "msg": err })
+    }
+    
+}
 
-module.exports = { shortlistedCandidateslist, getEligibileCandidateList };
+
+module.exports = { shortlistedCandidateslist, getEligibileCandidateList, renderShortlistedCandidate };
--- routes/route.js
+++ routes/route.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-const { 
-    getAllrecords,
-    createStudentRecord,
-    getPlacementData
+const {
+  getAllrecords,
+  createStudentRecord,
+  getPlacementData,
 } = require("../functions/user.operation");
 
-const { 
-    getAllCompanyrecords, createCompanyRecord, getAllCompanyname
+const {
+  getAllCompanyrecords,
+  createCompanyRecord,
+  getAllCompanyname,
 } = require("../functions/company.operation");
 
-const { shortlistedCandidateslist, getEligibileCandidateList } = require("../functions/company.interaction");
+const {
+  shortlistedCandidateslist,
+  getEligibileCandidateList,
+  renderShortlistedCandidate,
+} = require("../functions/company.interaction");
 
 router.post("/record/alldata", getAllrecords);
 router.post("/record/createData", createStudentRecord);
 router.post("/company/alldata", getAllCompanyrecords);
 router.post("/company/createCompanyRecord", createCompanyRecord);
 router.post("/interaction/shortlistedList", shortlistedCandidateslist);
-router.post("/interaction/getEligibileCandidateList", getEligibileCandidateList);
+router.post("/interaction/getEligibileCandidateList", getEligibileCandidateList );
+router.post("/interaction/renderShortlisted", renderShortlistedCandidate);
 router.post("/company/getAllCompanyname", getAllCompanyname);
 router.post("/record/getPlacementData", getPlacementData);
 
