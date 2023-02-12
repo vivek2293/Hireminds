b916c84ac7bcc7a38c8c0b9fee1ff7ca5a7c48c4
piqueblinder <preetkhatri07@gmail.com>
2023-02-12 19:51:41 +0530

    Added another option on testing file

--- src/Components/testing.jsx
+++ src/Components/testing.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 
 export default function Testing() {
+    const [msg, setMsg] = React.useState(false);
+
+    function handleChange() {
+        var ex = document.getElementById("selection");
+        var valu = ex.value;
+        if (valu === "Students") {
+            setMsg(true);
+        }
+    }
+
     return (
         <>
             <div className="main-div row d-flex align-items-center justify-content-between">
                 <div className="col-md-8 row justify-content-center">
-                    <input type="text" style={{width: '50vw'}}/>
+                    <input type="text" style={{ width: '50vw' }} />
                 </div>
-                <div className="col-md-4 d-flex" style={{flexDirection:'row !important'}}>
+                <div className="col-md-4 d-flex" style={{ flexDirection: 'row !important' }}>
                     <span className="mx-4">Send this message to </span>
                     <div className="drop-down-div">
-                        <select id="selection">
+                        <select id="selection" onChange={handleChange}>
                             <option value="ToRecruiters">To Recruiters</option>
                             <option value="Students">Students</option>
                         </select>
+                        {msg && <select id="further">
+                            <option value="1">1st Year</option>
+                            <option value="2">2nd Year</option>
+                            <option value="3">3rd Year</option>
+                            <option value="4">4tg Year</option>
+                        </select>}
                     </div>
                 </div>
             </div>
