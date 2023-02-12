354f66dbf63d43b4bb504a41bd3beab3498cc81f
Rutwik Dhale <rutwik2514@gmail.com>
2023-02-12 08:07:36 +0530

    Homepage css updated

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faLinkedin, faTwitter, faSkype, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faMapLocationDot, faPhone, faMailReply } from "@fortawesome/free-solid-svg-icons";
+
+function Footer() {
+  return (
+    <>
+        <div className="footer-upperdiv pb-3">
+              <div className="d-flex row justify-content-around">
+                <div className="col-md-3" id="first-one">
+                  <div className="logo mt-5 mb-1"><h4 id="Main-head">Team Byte Bandits</h4></div>
+                  <div className="text my-3"><p id="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui non corrupti, possimus deserunt sunt maxime impedit magni vero vitae itaque.</p></div>
+                </div>
+                <div className="col-md-3">
+                  <div className="contact-me mt-5 mb-1">
+                    <h5 id="contact">Contact Us</h5>
+                  </div>
+                  <div className="social-links">
+                    <ul className="mt-4">
+                      <li className="random my-2"><FontAwesomeIcon icon={faMapLocationDot} className="icons media-links me-2"></FontAwesomeIcon>Via Vesu, Surat India</li>
+                      <li className="random my-2"><FontAwesomeIcon icon={faPhone} className="icons media-links me-2"></FontAwesomeIcon> Phone: (0039) 333 12 68 347</li>
+                      <li className="random my-2"><FontAwesomeIcon icon={faMailReply} className="icons media-links me-2"></FontAwesomeIcon>Email: hello@domain.com</li>
+                      <li className="random my-2"><FontAwesomeIcon icon={faSkype} className="icons media-links me-2"></FontAwesomeIcon>Skype: you_online</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="link mt-5 mb-1"><h5 id="links">Links</h5></div>
+                  <div className="content d-flex">
+                    <div className="col-md-6">
+                      <ul>
+                        <li className="mt-3 mb-2 lists">Home</li>
+                        <li className="my-2 lists">Feature</li>
+                        <li className="my-2 lists">Method</li>
+                        <li className="my-2 lists">Our Clients</li>
+                      </ul>
+                    </div>
+                    <div className="col-md-6">
+                      <ul>
+                        <li className="mt-3 mb-2 lists">Plans & Pricing</li>
+                        <li className="my-2 lists">Affiliates</li>
+                        <li className="my-2 lists">Terms</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="footer-lowerdiv row py-3">
+              <div className="col-md-3 copyright">HireMinds Copyright © 2023 Byte Bandits</div>
+              <div className="col-md-3"></div>
+              <div className="col-md-3 row social-media" id="testing">
+                <ul id="display">
+                  <li className="m-2 handles"><FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></li>
+                  <li className="m-2 handles"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></li>
+                  <li className="m-2 handles"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></li>
+                  <li className="m-2 handles"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></li>
+                  <li className="m-2 handles"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></li>
+                </ul>
+              </div>
+            </div>
+    </>
+  )
+}
+
+export default Footer
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -4,13 +4,8 @@ import image2 from "./Assets/3.svg";
 import image3 from "./Assets/2.svg";
 import image4 from "./Assets/7.svg";
 import logo from "./Assets/Logo.png";
-import Lottie from "lottie-react";
-
-import animatelogo from "./Assets/logo.json"
 import "./CSS/Homepage.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faLinkedin, faTwitter, faSkype, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faMapLocationDot, faPhone, faMailReply } from "@fortawesome/free-solid-svg-icons";
+import Footer from "./Footer";
 
 function Homepage() {
   return (
@@ -29,20 +24,18 @@ function Homepage() {
             className=" col-md-12 d-flex justify-content-center align-items-start"
           >
             <div className="col-md-6" id="logo-div">
-              <img src={logo} style={{ height: '20vh' }} className="mt-2"></img>
-
+              <img src={logo} style={{ height: "20vh" }} className="mt-2"></img>
             </div>
             <div className="col-md-6" id="login-div">
-
               <div
                 className="btn-div d-flex justify-content-center mx-2 my-1"
                 id="btn-div1"
               >
                 <button
                   type="submit"
-                  className="btn btn-custom1 p-1 mt-1 shadow bg-white rounded"
-                  style={{ height: "6vh", width: "9vw" }}
-                  onClick={() => window.location.href = "/login"}
+                  className="btn btn-outline-light p-1 mt-2"
+                  style={{ height: "6vh", width: "9vw", fontWeight: "bold" }}
+                  onClick={() => (window.location.href = "/login")}
                 >
                   Login
                 </button>
@@ -51,12 +44,20 @@ function Homepage() {
                 className="btn-div d-flex justify-content-center mx-2 my-1"
                 id="btn-div2"
               >
-                <button type="submit"
-                  className="btn btn-custom1 p-1 mt-1 shadow bg-white rounded"
+                {/* <button type="submit"
+                  className="btn btn-custom3 p-1 mt-2 shadow bg-white rounded"
                   style={{ height: "6vh", width: "9vw" }}
                   onClick={() => window.location.href = "/register"}
                   >
                   Register
+                </button> */}
+                <button
+                  type="submit "
+                  className="btn btn-outline-light p-1 mt-2 "
+                  style={{ height: "6vh", width: "9vw", fontWeight: "bold" }}
+                  onClick={() => (window.location.href = "/register")}
+                >
+                  Signup
                 </button>
               </div>
             </div>
@@ -65,11 +66,24 @@ function Homepage() {
             <div className="row ">
               <div className="col-md-7 example-div">
                 <div className="head-div">
-                  <h1 className="ms-2 px-3 mt-1" style={{ color: 'rgb(120,123,206)' }}>Heading</h1>
+                  <h1
+                    className="ms-2 px-3 mt-1"
+                    style={{ color: "rgb(120,123,206)" }}
+                  >
+                    Heading
+                  </h1>
                 </div>
-                <div className="d-flex align-items-center mainpg-text-div" style={{ height: '70%' }}>
+                <div
+                  className="d-flex align-items-center mainpg-text-div"
+                  style={{ height: "70%" }}
+                >
                   <p className="ms-2 col-8 px-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deserunt sapiente dolore, ratione quam placeat voluptas nobis aspernatur esse animi accusamus dicta veritatis deleniti doloribus ea explicabo eos doloremque repellendus quasi eligendi autem totam sed. Quasi similique facere saepe quidem!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Odit deserunt sapiente dolore, ratione quam placeat voluptas
+                    nobis aspernatur esse animi accusamus dicta veritatis
+                    deleniti doloribus ea explicabo eos doloremque repellendus
+                    quasi eligendi autem totam sed. Quasi similique facere saepe
+                    quidem!
                   </p>
                 </div>
               </div>
@@ -84,11 +98,24 @@ function Homepage() {
               <div className="col-md-7 justify-content-end">
                 <div className="row justify-content-end">
                   <div className="head-div1 d-flex justify-content-center">
-                    <h1 className="ms-2 mb-3 col-8 px-3" style={{ color: 'rgb(120,123,206)' }}>Heading</h1>
+                    <h1
+                      className="ms-2 mb-3 col-8 px-3"
+                      style={{ color: "rgb(120,123,206)" }}
+                    >
+                      Heading
+                    </h1>
                   </div>
-                  <div className="d-flex justify-content-center align-items-center mainpg-text-div1" style={{ height: '25vh' }}>
+                  <div
+                    className="d-flex justify-content-center align-items-center mainpg-text-div1"
+                    style={{ height: "25vh" }}
+                  >
                     <p className="ms-2 col-8 px-3">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deserunt sapiente dolore, ratione quam placeat voluptas nobis aspernatur esse animi accusamus dicta veritatis deleniti doloribus ea explicabo eos doloremque repellendus quasi eligendi autem totam sed. Quasi similique facere saepe quidem!
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Odit deserunt sapiente dolore, ratione quam placeat
+                      voluptas nobis aspernatur esse animi accusamus dicta
+                      veritatis deleniti doloribus ea explicabo eos doloremque
+                      repellendus quasi eligendi autem totam sed. Quasi
+                      similique facere saepe quidem!
                     </p>
                   </div>
                 </div>
@@ -97,11 +124,24 @@ function Homepage() {
             <div className="row ">
               <div className="col-md-7 ">
                 <div className="head-div2">
-                  <h1 className="ms-2 px-3" style={{ color: 'rgb(120,123,206)' }}>Heading</h1>
+                  <h1
+                    className="ms-2 px-3"
+                    style={{ color: "rgb(120,123,206)" }}
+                  >
+                    Heading
+                  </h1>
                 </div>
-                <div className="d-flex align-items-center mainpg-text-div2" style={{ height: '70%' }}>
+                <div
+                  className="d-flex align-items-center mainpg-text-div2"
+                  style={{ height: "70%" }}
+                >
                   <p className="ms-2 col-8 px-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deserunt sapiente dolore, ratione quam placeat voluptas nobis aspernatur esse animi accusamus dicta veritatis deleniti doloribus ea explicabo eos doloremque repellendus quasi eligendi autem totam sed. Quasi similique facere saepe quidem!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Odit deserunt sapiente dolore, ratione quam placeat voluptas
+                    nobis aspernatur esse animi accusamus dicta veritatis
+                    deleniti doloribus ea explicabo eos doloremque repellendus
+                    quasi eligendi autem totam sed. Quasi similique facere saepe
+                    quidem!
                   </p>
                 </div>
               </div>
@@ -109,60 +149,7 @@ function Homepage() {
                 <img src={image4}></img>
               </div>
             </div>
-            <div className="footer-upperdiv pb-3">
-              <div className="d-flex row justify-content-around">
-                <div className="col-md-3" id="first-one">
-                  <div className="logo mt-5 mb-1"><h4 id="Main-head">Team Byte Bandits</h4></div>
-                  <div className="text my-3"><p id="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui non corrupti, possimus deserunt sunt maxime impedit magni vero vitae itaque.</p></div>
-                </div>
-                <div className="col-md-3">
-                  <div className="contact-me mt-5 mb-1">
-                    <h5 id="contact">Contact Us</h5>
-                  </div>
-                  <div className="social-links">
-                    <ul className="mt-4">
-                      <li className="random my-2"><FontAwesomeIcon icon={faMapLocationDot} className="icons media-links me-2"></FontAwesomeIcon>Via Vesu, Surat India</li>
-                      <li className="random my-2"><FontAwesomeIcon icon={faPhone} className="icons media-links me-2"></FontAwesomeIcon> Phone: (0039) 333 12 68 347</li>
-                      <li className="random my-2"><FontAwesomeIcon icon={faMailReply} className="icons media-links me-2"></FontAwesomeIcon>Email: hello@domain.com</li>
-                      <li className="random my-2"><FontAwesomeIcon icon={faSkype} className="icons media-links me-2"></FontAwesomeIcon>Skype: you_online</li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="col-md-3">
-                  <div className="link mt-5 mb-1"><h5 id="links">Links</h5></div>
-                  <div className="content d-flex">
-                    <div className="col-md-6">
-                      <ul>
-                        <li className="mt-3 mb-2 lists">Home</li>
-                        <li className="my-2 lists">Feature</li>
-                        <li className="my-2 lists">Method</li>
-                        <li className="my-2 lists">Our Clients</li>
-                      </ul>
-                    </div>
-                    <div className="col-md-6">
-                      <ul>
-                        <li className="mt-3 mb-2 lists">Plans & Pricing</li>
-                        <li className="my-2 lists">Affiliates</li>
-                        <li className="my-2 lists">Terms</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="footer-lowerdiv row py-3">
-              <div className="col-md-3 copyright">HireMinds Copyright © 2023 Byte Bandits</div>
-              <div className="col-md-3"></div>
-              <div className="col-md-3 row social-media" id="testing">
-                <ul id="display">
-                  <li className="m-2 handles"><FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></li>
-                  <li className="m-2 handles"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></li>
-                  <li className="m-2 handles"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></li>
-                  <li className="m-2 handles"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></li>
-                  <li className="m-2 handles"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></li>
-                </ul>
-              </div>
-            </div>
+            <Footer />
           </div>
         </div>
       </section>
